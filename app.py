f77d09666331dc79fb36dcb3789930ba8731f633
shania-codes <sh@ni.a>
2025-11-15 16:47:13 +0000

    switch to gtk3 because the docs will probably be better and it's better supported on XFCE

--- app.py
+++ app.py
@@ -1,40 +1,24 @@
-import sys
 import gi
-gi.require_version("Gtk", "4.0")
+
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-# https://pygobject.gnome.org/tutorials/gtk4/introduction.html
-
-# TODO is Gtk4 ugly on KDE/XFCE?
-# TODO add something that makes it not work on Windows and Mac
 
 
-def on_activate(self):
-    win = Gtk.ApplicationWindow(application=app)
-    win.set_default_size(800,600)
+class TheWhiteRoom(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="The White Room")
+        self.set_default_size(640,480) # TODO: Make it 16 colours only
 
-    dashboardbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-    win.set_child(dashboardbox)
+        self.invspcbtn = Gtk.Button(label="Inventory Space", margin=200)
+        self.invspcbtn.connect("clicked", self.invspcbtn_clicked)
+        self.add(self.invspcbtn)
 
-    label = Gtk.Label(label='Welcome to The White Room', halign=Gtk.Align.CENTER, valign=Gtk.Align.START, margin_top=15)
-    dashboardbox.append(label)
-
-    button = Gtk.Button(label="Inventory Space") # TODO Takes up max width
-    button.connect('clicked', inventoryspace)
-    dashboardbox.append(button)
-
-    
-
-    win.present()
+    def invspcbtn_clicked(self, widget):
+        # Make it "redirect" to a new "page"
 
 
-def inventoryspace(button):
-    inventoryspacebox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-    win.set_child(inventoryspacebox)
-    
 
-
-# Craete a new application
-app = Gtk.Application(application_id="dev.sapphire.thewhiteroom")
-app.connect("activate", on_activate)
-# Run the application
-app.run(None) 
+win = TheWhiteRoom()
+win.connect("destroy", Gtk.main_quit) # Close the program when the x button is pressed
+win.show_all()
+Gtk.main()
